5ddecc251f1d9deccb4701f34b23ec25cdbeb492
tuamor <cami@gmail.com>
2025-04-24 21:20:40 -0300

    (casi)fin 4 - Memoria diamica

--- src/4 - Memoria dinámica/Memoria.asm
+++ src/4 - Memoria dinámica/Memoria.asm
@@ -20,44 +20,90 @@ global strLen
 
 strCmp:
 	.loop:
-	; *a == *b == 0 -> .eq
-	mov al, [rdi]
-	mov bl, [rsi]
-	cmp al, bl
+	mov byte al, [rdi] ; guardamos en registro de 1 byte
+	mov byte cl, [rsi]
+	cmp al, cl 
 	jg .greater
 	jl .smaller
-	cmp al, 0
-	je .eq
+	cmp al, 0 
+	je .eq 	; *a == *b == 0 -> .eq
+	inc rdi
+	inc rsi
 	jmp .loop
 	
 	.smaller:
-	mov rax, -1
+	mov eax, 1
 	jmp .fin
 
 	.greater:
-	mov rax, 1
+	mov eax, -1
 	jmp .fin
 
 	.eq:
-	xor rax, rax
-	
+	xor eax, eax
+
 	.fin:
 	ret
 
+; GUARDA: NO ROMPER REGISTROS NO VOLÁTILES (ej: no podríamos haber usado el registro bl)
+; nos deberíamos asegurar de preservarlos (push bla bla)
+
 ; char* strClone(char* a)
 strClone:
+	push rbp
+	mov rbp, rsp
+
+	; quiero conservar r12
+	push r12
+	mov r12, rdi
+
+	call strLen
+	mov rdi, rax
+	inc rdi
+	call malloc ; rax = puntero al inicio de la mem reservada
+
+	mov rdx, rax
+
+	.loop:
+	mov cl, [r12]
+	mov [rdx], cl ; escribo el primer byte
+	cmp cl, 0
+	je .fin
+	
+	inc rdx
+	inc r12
+	jmp .loop
+
+	.fin:
+	pop r12
+	pop rbp
 	ret
 
 ; void strDelete(char* a)
 strDelete:
+	push rbp
+	mov rbp, rsp
+
+	call free
+
+	pop rbp
 	ret
 
 ; void strPrint(char* a, FILE* pFile)
 strPrint:
+	
 	ret
 
 ; uint32_t strLen(char* a)
 strLen:
+	xor rax, rax
+	.loop:
+	mov byte cl, [rdi]
+	cmp cl, 0
+	je .fin
+	inc eax
+	inc rdi
+	jmp .loop
+
+	.fin:
 	ret
-
-
